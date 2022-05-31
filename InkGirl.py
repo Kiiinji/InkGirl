7b155f61d9c423f1e00ccfaff526788208140cda
Kinji <39131872+Kiiinji@users.noreply.github.com>
2022-06-01 01:37:22 +0200

    Update InkGirl.py

--- InkGirl.py
+++ InkGirl.py
@@ -14,6 +14,7 @@ from discord.ext.commands import Bot
 from discord import opus
 import discord
 import youtube_dl
+import ffmpeg
 
 
 def get_metadata(query):
@@ -24,6 +25,30 @@ def get_metadata(query):
         "source_address": "0.0.0.0",
         "quiet": True
     }).extract_info(query, download=False)['url']
+
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+
+    if opus.is_loaded():
+
+        return True
+
+    for opus_lib in opus_libs:
+
+        try:
+
+            opus.load_opus(opus_lib)
+
+            return
+
+        except OSError:
+
+            pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
+
+#load_opus_lib()
 
 
 bot = commands.Bot(command_prefix='=')
